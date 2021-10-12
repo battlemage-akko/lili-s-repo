64d0046b9953aebdfe740e1797ac3c3238b74f94
battlemage-akko <421609385@qq.com>
2021-10-12 14:16:03 +0800

    4

---
+++ static/js/myvideo.js
@@ -0,0 +1,16 @@
+var danmulistbtn = document.querySelector('.danmubtn')
+var danmu = document.querySelector(".danmuholder")
+var video = document.querySelector('.thisvideo')
+var danmulist = document.querySelector('.danmulist')
+var danmuflag = 0
+danmulistbtn.addEventListener("click", (e) => {
+    danmulistbtn.classList.toggle("active")
+    if (!danmuflag) {
+        danmu.style.height = video.clientHeight + 50 + 'px'
+        danmuflag = 1
+        danmulist.style.height = video.clientHeight + 50 + 'px'
+    } else {
+        danmuflag = 0
+        danmu.style.height = 50 + 'px'
+    }
+})
