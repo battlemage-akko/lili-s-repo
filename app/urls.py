b47a5e3e1504bb457753fca7cf9bb9818bec8040
battlemage-akko <421609385@qq.com>
2021-10-14 02:28:13 +0800

    update

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import re_path, include
 from app import views
 
 urlpatterns = [
-    re_path('^login_check/',views.login_check,name="login_check")
+    re_path('^login_check/',views.login_check,name="login_check"),
+    re_path('^register/',views.register,name="register")
 ]
--- app/views.py
+++ app/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from app.models import AppUser as Userdatabase
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from myproject import urls
 
+@login_required
 def test(request):
     return render(request,'test.html')
 def index(request):
@@ -20,20 +22,42 @@ def wrapper(request):
 def login_check(request):
     if (request.method == 'GET'):
         pass
-    if(request.method == 'POST'):
+    if(request.method == 'POST' and request.POST):
         context = {
             'message': ''
         }
         username = request.POST.get("sign_in_Username")
         password = request.POST.get("sign_in_Password")
-        print(password)
         thisuser = authenticate(username=username,password=password)
-        print(thisuser)
         try:
             if thisuser:
                 return redirect("index")
         except AttributeError:
             return HttpResponse("出错了")
 
-        return HttpResponse("done")
+        return redirect("login")
+
+@csrf_exempt
+def register(request):
+    if (request.method == 'GET'):
+        pass
+    if request.method == 'POST' and request.POST:
+        username = request.POST.get("sign_up_Username")
+        password = request.POST.get("sign_up_Password")
+        email = request.POST.get("sign_up_Email")
+        registercheck = Userdatabase.objects.filter(username=username)
+
+        if registercheck:
+            message = {
+                "msg": "用户存在"
+            }
+            return render(request, "login.html", context=message)
+        else:
+            user = Userdatabase.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            message = {
+                "msg": "账号创建成功,快来登录试试吧"
+            }
+            return render(request, "login.html", context=message)
+        return redirect("login")
 # Create your views here.
