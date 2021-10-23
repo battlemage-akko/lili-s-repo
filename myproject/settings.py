d1045edd5de7434eea1d7a056b669e1d0116d9d4
battlemage-akko <421609385@qq.com>
2021-10-23 18:20:05 +0800

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,8 +144,7 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = '/data/wwwroot/myproject/static'
-#STATIC_ROOT = '/data/wwwroot/myproject/static'
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR,"static")
