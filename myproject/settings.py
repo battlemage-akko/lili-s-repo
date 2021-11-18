7707f4e0532f1bd50d34ee574640b2c9c0937721
battlemage-akko <421609385@qq.com>
2021-11-18 17:52:24 +0800

    修复搜索页BUG

--- myproject/settings.py
+++ myproject/settings.py
@@ -146,6 +146,7 @@ LOGIN_URL = 'login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
