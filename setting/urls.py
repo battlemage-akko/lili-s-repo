b28bb228d18062233a884b513adc1967f757ae4e
battlemage-akko <421609385@qq.com>
2021-12-02 01:41:36 +0800

    增加修改密码功能

--- setting/urls.py
+++ setting/urls.py
@@ -2,7 +2,11 @@ from django.urls import re_path, include,path
 from setting import views
 
 urlpatterns = [
-    path('',views.setting_profile),
+    path('',views.setting_info),
     re_path('^setting_profile/', views.setting_profile,name="setting_profile"),
     re_path('^setting_avatar/', views.setting_avatar,name="setting_avatar"),
+    re_path('^setting_info/', views.setting_info,name="setting_info"),
+    re_path('^setting_password/', views.setting_password,name="setting_password"),
+    re_path('^password_check/', views.password_check,name="password_check"),
+    re_path('^change_password/', views.change_password,name="change_password"),
 ]
--- setting/views.py
+++ setting/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth import authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+User = get_user_model()
 
 @login_required
 def setting(request):
@@ -14,3 +19,39 @@ def setting_profile(request):
 @login_required
 def setting_avatar(request):
     return render(request,'setting_avatar.html')
+
+@login_required
+def setting_info(request):
+    return render(request,'setting_info.html')
+
+@login_required
+def setting_password(request):
+    return render(request,'setting_password.html')
+
+@csrf_exempt
+def password_check(request):
+    if(request.method == 'POST'):
+        username = request.POST.get('username')
+        password = request.POST.get('old_password')
+        print(username,password)
+        check_result = authenticate(username=username, password=password)
+        if check_result:
+            return JsonResponse({
+                'code':1
+            })
+        else:
+            return JsonResponse({
+                'code': 0
+            })
+@csrf_exempt
+def change_password(request):
+    if(request.method == 'POST'):
+        username = request.POST.get('username')
+        password = request.POST.get('new_password')
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+        logout(request)
+        return JsonResponse({
+            "code":1
+        })
